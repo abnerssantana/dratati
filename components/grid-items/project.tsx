015106f1c116b4f61b9a9ecfbedc70ccbe503935
Abner Santana <abnerss@outlook.com>
2024-12-13 22:35:11 -0300

    Q

--- components/grid-items/project.tsx
+++ components/grid-items/project.tsx
@@ -1,5 +1,5 @@
 import { GridItemInterface } from "@/config/site-config";
-import { Baby, Heart, ContactRound, Cake } from "lucide-react"; // Importe os ícones necessários
+import { Baby, Heart, ContactRound, Cake, LucideIcon } from "lucide-react";
 
 const iconMap: { [key: string]: LucideIcon } = {
   baby: Baby,
@@ -10,10 +10,10 @@ const iconMap: { [key: string]: LucideIcon } = {
 
 const Project = ({ item }: { item: GridItemInterface }) => {
   const IconComponent = item.icon && iconMap[item.icon] ? iconMap[item.icon] : null;
-
+  
   return (
     <div className="flex items-center justify-between gap-4 pb-2">
-      {IconComponent && <div className="dark:text-neutral-50"><IconComponent /></div>} 
+      {IconComponent && <div className="dark:text-neutral-50"><IconComponent /></div>}
       <div className="w-full @lg:text-lg font-semibold">{item.title}</div>
       <div className="flex items-center gap-1">
         <div className="mt-[1px]">{item.stars}</div>
