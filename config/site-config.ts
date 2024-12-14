2f96a0547a37d5e853a678333ea7b005b7df228b
Abner Santana <abnerss@outlook.com>
2024-12-13 22:41:29 -0300

    fix

--- config/site-config.ts
+++ config/site-config.ts
@@ -89,7 +89,7 @@ const GridItems: GridItemInterface[] = [
     layout: "2x1",
     type: "project",
     title: "Foco em prevenção e cuidados desde a primeira infância",
-    icon: "guitar",
+    icon: "Cake",
     color: "#FFFFFF",
     backgroundColor: {
       light: "#B4D7BC",  // Verde suave
